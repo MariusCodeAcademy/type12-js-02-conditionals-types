ca92d4d4e122e5b8e51b07113b8bf4c62004267c
Marius <marius@example.com>
2022-07-05 13:02:46 +0300

    template literals

---
+++ backtick-notation.js
@@ -0,0 +1,36 @@
+let username = 'James';
+let age = 28;
+let town = 'Finland';
+
+// Student James is 22 years old and he lives in Minsk.
+let sentence =
+  'Student ' + username + ' is ' + age + ' years old and he lives in ' + town + '.';
+// ES6 Template Literals
+sentence = `Student ${username} is ${age} years old and he lives in ${town}.`;
+console.log('sentence ===', sentence);
+
+/*
+cia yra pirma eilute.
+cia yra antra. 
+paskutinte eilute.
+*/
+
+let str = '';
+str += 'cia yra pirma eilute.';
+str += 'cia yra antra.';
+str += 'paskutinte eilute.';
+
+str = `
+cia yra pirma eilute.
+cia yra antra. 
+paskutinte eilute.
+`;
+
+console.log('str ===', str);
+
+// suzinoti kiek metu bus usernamui po 5 metu
+// template literals galimos matematines operacijos
+let after5years = `${username} will be ${age + 5} in 5 years`;
+console.log('after5years ===', after5years);
+
+// gauti is vartotojo amziaus pokyti ir atspausdinti kiek bus meto po to pokycio
--- prompt.js
+++ prompt.js
@@ -1,1 +1,29 @@
 console.log('prompt.js is loaded');
+
+// apskaiciuti apskritimo plota
+
+let r = 10;
+let pi = 3.14;
+// pi * (r * r)
+
+let plotas = pi * (r * r);
+
+let resultString =
+  'Jei spindulys yra ' + r + ' tai apskritimo plotas yra ' + plotas + '.';
+console.log(resultString);
+
+// ivedam kita spinduli
+r = prompt('koks jusu apskritimo spindulys?');
+plotas = pi * (r * r);
+resultString = `Jei spindulys yra ${r} tai apskritimo plotas yra ${plotas}.`;
+console.log(resultString);
+
+// iskviesti ivedimo dialoga
+// prompt('kiek dabar valandu');
+// let valados = prompt('kiek dabar valandu');
+// console.log('valados ===', valados);
+// ekstra pranesimas vartotojui
+// alert('kazkas kazur kazkaip');
+
+// ekstra patvirtinimas
+// confirm('ar tikrai norite istrinti faila');
