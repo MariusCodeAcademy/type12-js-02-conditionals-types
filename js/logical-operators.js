274171f0133e33c0aafb579122b47199dfc5bee6
Marius <marius@example.com>
2022-07-05 15:04:36 +0300

    ifs

--- js/logical-operators.js
+++ js/logical-operators.js
@@ -1,9 +1,17 @@
 // Sudetines salygos
 
-// loginis AND
+// loginis AND &&
 console.log('true && true ===', true && true);
 console.log('true && false ===', true && false);
 console.log('false && true ===', false && true);
 console.log('false && false ===', false && false);
 
-// loginis OR
+// loginis OR ||
+console.log('true || true ===', true || true);
+console.log('true || false ===', true || false);
+console.log('false || true ===', false || true);
+console.log('false || false ===', false || false);
+
+// loginis NOT !
+console.log('!true ===', !true);
+console.log('!false ===', !false);
